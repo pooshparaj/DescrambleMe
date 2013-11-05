bf14a9bf8e0a73d949652d0043a4d78e0dceab8f
pooshparaj <pooshparaj.pandurangan@gmail.com>
2013-11-05 23:28:09 +0530

    phrases added  to description.js

--- viewcontrollers/description.js
+++ viewcontrollers/description.js
@@ -14,7 +14,7 @@ var parseString = require('xml2js').parseString;
 
 module.exports.controller = function(app){
 	app.get('/description',function(req,res){
-		getWordDetails("cqAFg509aw", res, "dog");
+		getWordDetails("cqAFg509aw", res, "rose");
 	});
 };
 
@@ -59,7 +59,36 @@ function getDKImages(value,res,obj)
 			});
 			response.on('end', function() {	
 				imagecollection = JSON.parse(body);
-				var obj = JSON.parse(wordlist);							
+				getPhrases(value,res,imagecollection,obj);
+			});
+		});
+		request.on('error', function(e) {
+		console.log('Problem with request: ' + e.message);
+		});
+		request.end();
+}
+
+
+function getPhrases(value,res,imagecollection,obj) 
+{		var http = require("http");
+		var options = {
+			host : 'stands4.com',
+			path : '/services/v2/phrases.php?uid=2167&tokenid=LL870U6UWvQIh9w&phrase='+value,
+			port : 80,
+			method : 'GET'
+		}
+		var request = http.get(options, function(response){
+			var body = ""
+			response.on('data', function(data) {
+			  body += data;
+			});
+			
+			response.on('end', function() {
+				var phraseObj = "";
+				parseString(body, function (err, result) {
+					phraseObj = result;	
+				});	
+				
 				keyWord = obj.results[0].headword;
 				Definition = obj.results[0].senses[0].definition;
 				if(obj.results[0].senses[0].gramatical_examples)
@@ -69,13 +98,15 @@ function getDKImages(value,res,obj)
 				var image = "";
 				image = imagecollection.images[0].url+","+imagecollection.images[1].url+","+imagecollection.images[2].url			
 
-				var collection = {'KeyWord':keyWord,'Audio':Audio,'Definition':Definition,'Usage':Usage,'Phrase':Phrase,'Synonyms':Synonyms, 'Anagrams':Anagrams,'Images':image}
+				var collection = {'KeyWord':keyWord,'Audio':Audio,'Definition':Definition,'Usage':Usage,'Phrase':phraseObj.results.result[0].example,'Synonyms':Synonyms, 'Anagrams':Anagrams,'Images':image}
 				
-				res.render('description/description',{data:collection})
+				res.render('description/description',{data:collection})	
+				
+				
 			});
 		});
 		request.on('error', function(e) {
-		console.log('Problem with request: ' + e.message);
+			console.log('Problem with request: ' + e.message);
 		});
 		request.end();
 }
