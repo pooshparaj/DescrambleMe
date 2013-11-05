0ad6a8a7053a4daf17303789b8ea3b2ce8196946
pooshparaj <pooshparaj.pandurangan@gmail.com>
2013-11-06 02:54:41 +0530

    include anagrams

--- viewcontrollers/description.js
+++ viewcontrollers/description.js
@@ -14,7 +14,8 @@ var parseString = require('xml2js').parseString;
 
 module.exports.controller = function(app){
 	app.get('/description',function(req,res){
-		getWordDetails(res, "compute");
+		//console.log('Problem with request: ' + req.query.word);
+		getWordDetails(res, req.query.word);
 	});
 };
 
@@ -88,27 +89,68 @@ function getPhrases(value,res,imagecollection,obj)
 				parseString(body, function (err, result) {
 					phraseObj = result;	
 				});
+				getSynonyms(value,res,imagecollection,obj,phraseObj)				
+			});
+		});
+		request.on('error', function(e) {
+			console.log('Problem with request: ' + e.message);
+		});
+		request.end();
+}
+
+function getSynonyms(value,res,imagecollection,obj,phraseObj)
+{
+	var http = require("http");
+		var options = {
+			host : 'anagramica.com',
+			path : '/best/:'+value,
+			port : 80,
+			method : 'GET'
+		}
+		var request = http.get(options, function(response){
+			var body = ""
+			response.on('data', function(data) {
+			  body += data;
+			});
+			
+			response.on('end', function() {
+				var data= JSON.parse(body);
+				var letter = "";
+				for (var l = 0; l < data.best.length; l++) {
+					if(l == data.best.length-1)
+						letter  = letter + data.best[l];
+						else
+						letter  = letter + data.best[l] + ","
+				}		
+			
 				if(Object.keys(obj).length > 0)
 				{						
-					for (var item = 0; item < Object.keys(obj).length; item++) {
+					for (var item = 0; item < obj.results.length; item++) {
 							if(obj.results[item].headword == value)
 							{	
 								keyWord = obj.results[item].headword;
 								Definition = obj.results[item].senses[0].definition;
+								console.log('Problem with request: ' + Definition);
 								if(obj.results[item].senses[0].examples)
 								{						
 								Usage = obj.results[item].senses[0].examples[0].text;
 								if(obj.results[item].senses[0].examples[0].audio)
-								Audio = obj.results[item].senses[0].examples[0].audio[0].url;
-								}						
+								Audio = "http://api.pearson.com"+obj.results[item].senses[0].examples[0].audio[0].url;
+								}
+								item = obj.results.length +1;
 							}
 					}
-				}				
+				}
+				console.log('Problem with request: ' + JSON.stringify(phraseObj));
+				if(phraseObj.results.result){
+					Phrase = phraseObj.results.result[0].explanation
+				}
+				
 				var image = "";
 				if(imagecollection.length != null)
 				image = imagecollection.images[0].url+","+imagecollection.images[1].url+","+imagecollection.images[2].url			
 
-				var collection = {'KeyWord':keyWord,'Audio':Audio,'Definition':Definition,'Usage':Usage,'Phrase':phraseObj.results.result[0].example,'Synonyms':Synonyms, 'Anagrams':Anagrams,'Images':image}
+				var collection = {'KeyWord':keyWord,'Audio':Audio,'Definition':Definition,'Usage':Usage,'Phrase':Phrase,'Synonyms':Synonyms, 'Anagrams':letter,'Images':image}
 				
 				res.render('description/description',{data:collection})				
 				
