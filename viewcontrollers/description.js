532941d0f11e29028d1a376aefc585128f5ed9c2
pooshparaj <pooshparaj.pandurangan@gmail.com>
2013-11-05 19:57:19 +0530

    new controller added for description view

---
+++ viewcontrollers/description.js
@@ -0,0 +1,90 @@
+var http = require("http");
+var wordlist = "";
+var imagecollection = "";
+var keyWord = "";
+var Audio = "";
+var Definition = ""
+var Usage = "";
+var Phrase = "";
+var Synonyms="";
+var Anagrams = "";
+var Images = "";
+var parseString = require('xml2js').parseString;
+
+
+module.exports.controller = function(app){
+	app.get('/description',function(req,res){
+		getWordDetails("cqAFg509aw", res, "dog");
+	});
+};
+
+
+function getWordDetails(id,res,value) 
+{		var http = require("http");
+		var options = {
+			host : 'api.pearson.com',
+			path : '/v2/dictionaries/entries?search='+id+'&apikey=6pUM7idZK2khzpx31xSfUoUapA2wQbzm',
+			port : 80,
+			method : 'GET'
+		}
+		var request = http.request(options, function(response){
+			var body = ""
+			response.on('data', function(data) {
+			  body += data;
+			  wordlist = body;
+			});
+			response.on('end', function() {	
+				wordlist = body;
+				getDKImages(value,res,JSON.parse(body));
+			});
+		});
+		request.on('error', function(e) {
+		console.log('Problem with request: ' + e.message);
+		});				
+		request.end();
+}
+
+function getDKImages(value,res,obj) 
+{		var http = require("http");
+		var options = {
+			host : 'api.pearson.com',
+			path : '/dk/v1/images?caption='+value+'&limit=3&apikey=6pUM7idZK2khzpx31xSfUoUapA2wQbzm',
+			port : 80,
+			method : 'GET'
+		}
+		var request = http.request(options, function(response){
+			var body = ""
+			response.on('data', function(data) {
+			  body += data;
+			});
+			response.on('end', function() {	
+				imagecollection = JSON.parse(body);
+				var obj = JSON.parse(wordlist);							
+				keyWord = obj.results[0].headword;
+				Definition = obj.results[0].senses[0].definition;
+				if(obj.results[0].senses[0].gramatical_examples)
+				{						
+					Usage = obj.results[0].senses[0].gramatical_examples[0].examples[0].text;
+				}
+				var image = "";
+				image = imagecollection.images[0].url+","+imagecollection.images[1].url+","+imagecollection.images[2].url			
+
+				var collection = {'KeyWord':keyWord,'Audio':Audio,'Definition':Definition,'Usage':Usage,'Phrase':Phrase,'Synonyms':Synonyms, 'Anagrams':Anagrams,'Images':image}
+				
+				res.render('description/description',{data:collection})
+			});
+		});
+		request.on('error', function(e) {
+		console.log('Problem with request: ' + e.message);
+		});
+		request.end();
+}
+
+module.exports.socket = function(socket){
+	
+	socket.on('message',function(data){
+		socket.broadcast.emit('message',data);	
+	});
+
+	
+};
